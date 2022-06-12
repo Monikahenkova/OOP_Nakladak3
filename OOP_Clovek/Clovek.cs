a266023a8416ca71b28be53c0333da870df25189
Monika Hénková <Henkova.Monika@stud.vassboskovice.cz>
2022-06-12 22:17:14 +0200

    Přidejte soubory projektu.

---
+++ OOP_Clovek/Clovek.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Clovek
+{
+    public class Nakladak
+    {
+        // soukromé proměnné - nejsou dostupné mimo třídu Nakladak
+        private int palivo;
+        private int naklad;
+        private int dojezd;
+
+        // vlastnosti - zveřejňují hodnoty ven z třídy
+        public string Auto { get; set; }
+        public string Znacka { get; set; }
+        public int Palivo { get => palivo; }
+        public int Dojezd { get => dojezd; }
+        public int Nosnost { get; set; }
+        public int Naklad { get => naklad; }
+        public string Odjezd { get; set; }
+
+        //konstruktor - pomocí této metody vznikne konkrétní objekt - instance třídy
+
+        public Nakladak()
+        {
+            Znacka = "";
+            Auto = "";
+            palivo = 200;
+            dojezd = 1000;
+            Nosnost = 12000;
+            naklad = 0;
+            Odjezd = "Jedeš!";
+        }
+        public void Nalozitnaklad()
+        {
+
+            if (naklad <= 11000)
+            {
+                naklad += 1000;
+            }
+            else if (naklad > 11000)
+            {
+                    Odjezd = "Víc nenaložíš!";
+            }
+        }
+        public void Vylozitnaklad()
+        {
+
+            if (naklad == 0)
+            {
+                naklad = 0;
+            }
+            else if (naklad > 0)
+            {
+                naklad -= 1000;
+            }
+        }
+
+        public void Natankovat()
+        {
+
+            if (palivo <= 150)
+            {
+                palivo += 50;
+            }
+            else if (palivo >= 200)
+            {
+                palivo = 200;
+            }
+        }
+        public void Jed()
+        {  
+            if (naklad <= 12000)
+            {
+                palivo -= 50;
+            }
+            else if (palivo <= 0)
+            {
+                palivo = 0;
+            }
+            if (palivo <= 0)
+            {
+                Odjezd = "Nejedeš!";
+            }
+        }
+
+      
+    }
+}
---
+++ OOP_Clovek/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_Clovek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Nakladak Fiat;
+        Nakladak Volvo;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Fiat = new Nakladak();
+            Fiat.Auto = "Fiat";
+            Fiat.Znacka = "Ducato";
+            Zobraz(Fiat, txt1);
+
+            Volvo = new Nakladak();
+            Volvo.Auto = "Volvo";
+            Volvo.Znacka = "FH16";
+            Zobraz(Volvo, txt2);
+        }
+
+        public void Zobraz(Nakladak nakladak, TextBox textBox)
+        {
+            textBox.Text = nakladak.Auto + " " + nakladak.Znacka + "\n";
+            textBox.Text += "Palivo: " + nakladak.Palivo.ToString() + "\n";
+            textBox.Text += "Dojezd: " + nakladak.Dojezd.ToString() + "\n";
+            textBox.Text += "Nosnost: " + nakladak.Nosnost.ToString() + "\n";
+            textBox.Text += "Naklad: " + nakladak.Naklad.ToString() + "\n";
+            textBox.Text += "Odjezd: " + nakladak.Odjezd.ToString() + "\n";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Fiat.Jed();
+            Zobraz(Fiat, txt1);
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            Fiat.Vylozitnaklad();
+            Zobraz(Fiat, txt1);
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            Fiat.Natankovat();
+            Zobraz(Fiat, txt1);  
+        }
+
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            Fiat.Nalozitnaklad();
+            Zobraz(Fiat, txt1);
+        }
+
+
+        private void Button_Click6(object sender, RoutedEventArgs e)
+        {
+            Volvo.Jed();
+            Zobraz(Volvo, txt2);
+        }
+
+        private void Button_Click7(object sender, RoutedEventArgs e)
+        {
+            Volvo.Vylozitnaklad();
+            Zobraz(Volvo, txt2);
+
+        }
+
+        private void Button_Click8(object sender, RoutedEventArgs e)
+        {
+            Volvo.Natankovat();
+            Zobraz(Volvo, txt2);
+        }
+
+
+        private void Button_Click10(object sender, RoutedEventArgs e)
+        {
+            Volvo.Nalozitnaklad();
+            Zobraz(Volvo, txt2);
+        }
+
+
+    }
+}
+
